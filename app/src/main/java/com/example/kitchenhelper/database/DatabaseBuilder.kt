b0d994658739cea41e5d5d74f0560b3c9f591ca5
Kenneth Heiserman <kennethheiserman@yahoo.com>
2023-05-12 04:21:29 -0500

    added DAO interfaces and companion object to DatabaseBuilder.kt
    Signed-off-by: Kenneth Heiserman <kennethheiserman@yahoo.com>

--- app/src/main/java/com/example/kitchenhelper/database/DatabaseBuilder.kt
+++ app/src/main/java/com/example/kitchenhelper/database/DatabaseBuilder.kt
@@ -1,4 +1,34 @@
 package com.example.kitchenhelper.database
 
-abstract class DatabaseBuilder{
+import android.content.Context
+import androidx.room.Room.databaseBuilder
+import androidx.room.RoomDatabase
+import com.example.kitchenhelper.dao.FoodItemDAO
+import com.example.kitchenhelper.dao.RecipeDAO
+import com.example.kitchenhelper.dao.UserDAO
+
+abstract class DatabaseBuilder : RoomDatabase() {
+    abstract fun userDAO(): UserDAO?
+    abstract fun foodItemDAO(): FoodItemDAO?
+    abstract fun recipeDAO(): RecipeDAO?
+
+    companion object {
+        @Volatile
+        private var INSTANCE: DatabaseBuilder? = null
+        fun getDatabase(context: Context): DatabaseBuilder? {
+            if (INSTANCE == null) {
+                synchronized(DatabaseBuilder::class.java) {//remove java to the end of class if it doesn't work right
+                    if (INSTANCE == null) {
+                        INSTANCE = databaseBuilder(
+                            context.applicationContext,
+                            DatabaseBuilder::class.java,//same here
+                            "myDatabase"
+                        )
+                            .fallbackToDestructiveMigration().build()
+                    }
+                }
+            }
+            return INSTANCE
+        }
+    }
 }
